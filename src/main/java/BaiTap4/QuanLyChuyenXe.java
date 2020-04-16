f15a9aca2b6b679281768316e90848da70f15aba
thoadinh <thoadinh1998@gmail.com>
2020-04-16 08:55:00 +0700

    ver1.0

--- src/main/java/BaiTap4/QuanLyChuyenXe.java
+++ src/main/java/BaiTap4/QuanLyChuyenXe.java
@@ -9,5 +9,47 @@ public class QuanLyChuyenXe {
     public void themChuyenXe(ChuyenXe cx){
         chuyenXes.add(cx);
     }
+    //Tong danh thu cho từng loại xe 
+    public double doanhThuNoiThanh(){
+        int sumNT = 0;
+        for(ChuyenXe xeNoiThanh : chuyenXes){
+            if(xeNoiThanh instanceof XeNoiThanh){
+                sumNT += xeNoiThanh.getDoanhThu();
+            }
+        }
+        return sumNT;
+    }
     
+    public double doanhThuNgoaiThanh(){
+       int temp = 0;
+       for(ChuyenXe xeNgoaiThanh : chuyenXes){
+            if(xeNgoaiThanh instanceof XeNgoaiThanh){
+                temp += xeNgoaiThanh.getDoanhThu();
+            }
+        }
+        return temp;
+    }
+    
+    public double tongDoanhThu(){
+        return doanhThuNgoaiThanh()+doanhThuNoiThanh();
+    }
+    
+    public void inDanhSach(){
+        for (ChuyenXe chuyenXe : chuyenXes) {
+            System.out.println("--------------------------");
+            System.out.println("Chuyen xe:" + chuyenXe.getMaChuyenXe());
+            System.out.println("Ho Ten Tai Xe: "+chuyenXe.getHoTenTaiXe());
+            System.out.println("Ma so xe: "+chuyenXe.getSoXe());
+            System.out.println("Doanh Thu: "+chuyenXe.getDoanhThu());
+            if(chuyenXe instanceof XeNoiThanh){
+                System.out.println("So Km: "+((XeNoiThanh) chuyenXe).getSoKm());
+                System.out.println("So Tuyen: "+((XeNoiThanh) chuyenXe).getSoTuyen());
+            }
+            if(chuyenXe instanceof XeNgoaiThanh){
+                System.out.println("So Ngay Di: "+ ((XeNgoaiThanh) chuyenXe).getSoNgayDi());
+                System.out.println("Noi Den: "+ ((XeNgoaiThanh) chuyenXe).getNoiDen());
+            }
+        }
+            
+    }
 }
--- src/main/java/BaiTap4/main4.java
+++ src/main/java/BaiTap4/main4.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BaiTap4;
 
-/**
- *
- * @author Dinh Van Thai
- */
 public class main4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+         XeNoiThanh xe1 = new XeNoiThanh(100,"Nha Trang - Ninh Hoa", "XeNoiThanh01", "Nguyen Van A", "123456", 120000);
+         XeNoiThanh xe2 = new XeNoiThanh(150,"Nha Trang - Van Gia", "XeNoiThanh02", "Nguyen Van B", "12345", 100000);
+         XeNoiThanh xe3 = new XeNoiThanh(200,"Nha Trang - Cam Ranh", "XeNoiThanh03", "Nguyen Van C", "1234", 150000);
+         
+         XeNgoaiThanh xe4 = new XeNgoaiThanh("Sai Gon", 1, "XeNgoaiThanh01", "Phan Van A", "12345", 1500000);
+         XeNgoaiThanh xe5 = new XeNgoaiThanh("Ha Noi", 2, "XeNgoaiThanh02", "Phan Van B", "123456", 100000);
+         XeNgoaiThanh xe6 = new XeNgoaiThanh("Da Nang", 3, "XeNgoaiThanh03", "Phan Van C", "1234578", 1200000);
+         
+         QuanLyChuyenXe qlcx = new QuanLyChuyenXe();
+         qlcx.themChuyenXe(xe1);
+         qlcx.themChuyenXe(xe2);
+         qlcx.themChuyenXe(xe3);
+         qlcx.themChuyenXe(xe4);
+         qlcx.themChuyenXe(xe5);
+         qlcx.themChuyenXe(xe6);
+         
+         System.out.println("Tong Doang Thu Xe Noi Thanh: "+ qlcx.doanhThuNoiThanh());
+         System.out.println("Tong Doanh Thu Xe Ngoai Thanh: "+qlcx.doanhThuNgoaiThanh());
+         System.out.println("Tong Doanh Thu: "+ qlcx.tongDoanhThu());
+         
+         qlcx.inDanhSach();
+         
     }
     
 }
